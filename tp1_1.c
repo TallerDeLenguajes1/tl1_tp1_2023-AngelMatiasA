653dfb5281d6b38a1aaac25fca5f3dbd8c4856e8
AngelMatiasA <angel.apaza010@gmail.com>
2023-03-23 18:12:40 -0300

    commit del punto 3

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,30 @@
 
 int main(){
 
+/*Declare un puntero en el procedimiento principal que apunte a una
+variable de algún tipo y muestre por pantalla lo siguiente:
+El contenido del puntero
+1) La dirección de memoria almacenada por el puntero.
+2) la dirección de memoria de la variable.
+3) la dirección de memoria del puntero.
+4) el tamaño de memoria utilizado por esa variable usando la
+función sizeof()
+5) El contenido del puntero
+6) La dirección de memoria almacenada por el puntero.
+7) La dirección de memoria de la variable.
+8) la dirección de memoria del puntero.
+*/
     printf("hola mundo");
+    int num1 = 0; 
+    int *pNum = &num1; 
+
+    printf(" \n el contenido del puntero es %d", *pNum);
+
+    printf(" \n la direccion de memoria almacenada por el puntero es de %d", pNum);
+    printf(" \n la direccion de memoria de la variable %d", &num1);
+    printf(" \n la direccion de memoria del puntero es %d", &pNum);
+    
+
 
     return 0;
 }
